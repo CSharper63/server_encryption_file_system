b3b628741e159e1e511dfb8b01caefa3ab2acb6c
Maxime <47356381+CSharper63@users.noreply.github.com>
2024-01-10 22:17:55 +0100

    update model to be same as the client

--- src/models.rs
+++ src/models.rs
@@ -1,30 +1,45 @@
 use std::{
+    collections::HashMap,
+    fmt::format,
     fs::{self, File, OpenOptions},
+    hash::Hash,
     io::{BufWriter, Write},
     path::Path,
 };
 
-use base64::{engine::general_purpose, Engine as _};
 use jsonwebtoken::{
     decode, encode, errors::Error, get_current_timestamp, Algorithm, DecodingKey, EncodingKey,
     Header, Validation,
-};
-use rocket::{
-    http::Status,
-    outcome::Outcome,
-    request::{self, FromRequest, Request},
-    FromForm,
 };
 
 use serde::{Deserialize, Serialize};
 
 const SERVER_ROOT: &str = "vault";
 const USERS_DB: &str = "users.json";
+const METADATA: &str = "metadata.json";
+const USERS_DIR: &str = "users";
+// vault/users/USER_ID/metadata.json/
+// vault/users/USER_ID/data/CIPHER_DIR
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct EncryptedAsset {
+pub struct Metadata {
+    dirs: Vec<DirEntity>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum DataStatus {
+    // allow to know if the current processed data are encrypted or not
+    Encrypted,
+    Decrypted,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct DataAsset {
+    // can contain encrypted/decrypted data
     pub asset: String,
     pub nonce: String,
+    #[serde(skip_serializing)]
+    pub status: Option<DataStatus>,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
@@ -32,26 +47,31 @@ pub struct User {
     pub uid: String,
     pub username: String,
     pub clear_salt: String,
-    pub encrypted_master_key: EncryptedAsset,
+    pub master_key: DataAsset,
     pub auth_key: String,
     pub public_key: String,
-    pub encrypted_private_key: EncryptedAsset,
+    pub private_key: DataAsset,
+    // contains the file/folder
+    pub shared_to_others: Option<HashMap<String, String>>,
+    pub shared_to_me: Option<HashMap<String, String>>,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct DirEntity {
     // must be sent to the client while logged in
     pub path: String,
-    pub encrypted_name: EncryptedAsset,
-    pub encrypted_key: EncryptedAsset,
+    pub name: DataAsset,
+    pub key: DataAsset,
     pub files: Vec<FileEntity>,
-    pub dirs: Vec<DirEntity>,
+    pub dirs: Option<Vec<DirEntity>>,
 }
 
 impl DirEntity {
     pub fn create(&self) -> bool {
         match fs::create_dir_all(&self.path) {
             Ok(_) => {
+                // create metadata
+
                 return true;
             }
             Err(e) => {
@@ -65,16 +85,11 @@ impl DirEntity {
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct FileEntity {
     pub path: String,
-    pub encrypted_name: EncryptedAsset,
-    pub encrypted_key: EncryptedAsset,
-    pub encrypted_content: EncryptedAsset,
+    pub name: DataAsset,
+    pub key: DataAsset,
+    pub content: DataAsset,
 }
-
 impl FileEntity {
-    pub fn decode_b64(str: &str) -> Vec<u8> {
-        general_purpose::STANDARD.decode(str).unwrap()
-    }
-
     pub fn create(&self) -> bool {
         let path = Path::new(&self.path);
 
@@ -86,7 +101,7 @@ impl FileEntity {
             }
         };
 
-        match file.write_all(&FileEntity::decode_b64(&self.encrypted_content.asset)) {
+        match file.write_all(self.content.asset.clone().as_bytes()) {
             Ok(_) => return true,
             Err(e) => {
                 eprintln!("Error while filling content : {}", e);
@@ -108,8 +123,12 @@ impl Database {
         format!("{}/{}", SERVER_ROOT, USERS_DB)
     }
 
+    pub fn get_user_metadata(uid: &str) -> String {
+        format!("{}/{}/{}/{}", SERVER_ROOT, USERS_DIR, uid, METADATA)
+    }
+
     fn get_user_bucket(uid: &str) -> String {
-        format!("{}/buckets/{}", SERVER_ROOT, uid)
+        format!("{}/{}/{}/bucket/", SERVER_ROOT, USERS_DIR, uid)
     }
 
     fn get_all_users() -> Result<Database, Box<dyn std::error::Error>> {
@@ -138,6 +157,70 @@ impl Database {
         }
     }
 
+    // use to update the user metadata while adding a new file
+    /* fn add_to_dir_tree(uid: &str, file_entity: FileEntity) {
+        // load content of the current metatadata for a "uid" user
+        let metadata_path = Database::get_user_metadata(uid);
+        let str_raw_metadata = std::fs::read_to_string(&metadata_path).unwrap();
+
+        // Deserialize string to Metadata struct
+        let mut metadata: Metadata =
+            serde_json::from_str(&str_raw_metadata).expect("Unable to deserialize metadata");
+
+        // Find the dir entity where add the file entity
+        let mut current_dir = &mut metadata.dirs;
+        for dir_name in file_entity.path.split('/') {
+            // check if not empty
+            if !dir_name.is_empty() {
+                // if dir has been found
+                if let Some(found_dir) = current_dir.iter_mut().find(|dir| {
+                    String::from_utf8_lossy(dir.name.asset.unwrap().as_slice()).to_string()
+                        == dir_name.to_string()
+                }) {
+                    current_dir = &mut found_dir.dirs.unwrap();
+                } /*  else {
+                        // check if needed
+                      let new_dir = DirEntity {
+                          path: format!("{}/{}", file_entity.path, dir_name),
+                          name: DataAsset {
+                              asset: Some(dir_name.into()),
+                              nonce: None,
+                              status: None,
+                          },
+                          key: DataAsset {
+                              asset: None,
+                              nonce: None,
+                              status: None,
+                          },
+                          files: Vec::new(),
+                          dirs: None,
+                      };
+                      current_dir.push(new_dir);
+                      current_dir = &mut current_dir.last_mut().unwrap().dirs.unwrap();
+                  } */
+            }
+        }
+
+        // add the file in the the folder
+        if let Some(last_dir) = current_dir.last_mut() {
+            last_dir.files.push(file_entity);
+        }
+
+        // update the metadata
+        let updated_metadata_content =
+            serde_json::to_string(&metadata).expect("Unable to serialize metadata");
+
+        let file = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .open(metadata_path)
+            .unwrap();
+
+        let mut writer = BufWriter::new(file);
+        serde_json::to_writer(&mut writer, &updated_metadata_content).unwrap();
+        writer.flush().unwrap();
+    } */
+
     pub fn get_user(username: &str) -> Option<User> {
         if let Ok(database) = Self::get_all_users() {
             for user in database.users {
@@ -147,6 +230,17 @@ impl Database {
             }
         }
         None
+    }
+
+    // use to get all files from a dir.
+    //
+    pub fn get_dir(path: DirEntity) {
+        // get name from this path, then get the metadata from dir
+        let server_root_path = Path::new(SERVER_ROOT);
+
+        if !server_root_path.exists() {
+            fs::create_dir_all(server_root_path).expect("Failed to create server root directory");
+        }
     }
 
     pub fn add_user(new_user: User) -> std::io::Result<()> {
@@ -183,7 +277,7 @@ impl Database {
         let file_path = format!("{}/{}", user_bucket, file.path);
 
         let mut sysfile = File::create(&file_path)?;
-        sysfile.write_all(&FileEntity::decode_b64(&file.encrypted_content.asset))?;
+        sysfile.write_all(&file.content.asset.as_bytes().to_vec())?;
 
         Ok(())
     }
@@ -227,8 +321,8 @@ impl Database {
             // update encrypted keys
             db_users.users[index].clear_salt = user_2_update.clear_salt;
             db_users.users[index].auth_key = user_2_update.auth_key;
-            db_users.users[index].encrypted_master_key = user_2_update.encrypted_master_key;
-            db_users.users[index].encrypted_private_key = user_2_update.encrypted_private_key;
+            db_users.users[index].master_key = user_2_update.master_key;
+            db_users.users[index].private_key = user_2_update.private_key;
 
             let file = OpenOptions::new()
                 .write(true)
