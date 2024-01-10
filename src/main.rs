ccb5e70304cfb1956515f830fb54d60b1e5e68b1
Maxime <47356381+CSharper63@users.noreply.github.com>
2024-01-10 22:18:28 +0100

    add todos, format json some endpoint

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ use rocket::*;
 use uuid::Uuid;
 // TODO !!!!! REMOVE ERROR FROM PAYLOAD RESPONSE
 /// Authentication status: None
-#[get("/get_salt/<username>")]
-pub fn get_salt(username: &str) -> status::Custom<String> {
+#[get("/auth/get_salt/<username>?<auth_key>")]
+pub fn get_salt(username: &str, auth_key: &str) -> status::Custom<String> {
     let generic_error = status::Custom(Status::BadRequest, "Unable to get salt".to_string());
 
     match Database::get_user(username) {
@@ -41,9 +41,11 @@ pub fn get_sign_in(username: &str, auth_key: &str) -> status::Custom<String> {
 }
 
 /// Authentication status: None
-#[get("/get_sign_up", data = "<new_user>")]
+#[get("/get_sign_up", format = "json", data = "<new_user>")]
 pub fn get_sign_up(new_user: &str) -> status::Custom<String> {
     let generic_error = status::Custom(Status::BadRequest, "Unable to sign up".to_string());
+
+    println!("fetched user: {}", new_user);
 
     // convert body to struct
     let mut new_user: User = match serde_json::from_str(new_user) {
@@ -73,7 +75,7 @@ pub fn get_sign_up(new_user: &str) -> status::Custom<String> {
         Ok(_) => {
             // after successfully add user, sent the JWT to access to the service
             match Database::generate_jwt(&new_user) {
-                Ok(jwt) => return status::Custom(Status::Ok, jwt),
+                Ok(jwt) => return status::Custom(Status::Created, jwt),
                 Err(_) => return generic_error,
             };
         }
@@ -81,15 +83,17 @@ pub fn get_sign_up(new_user: &str) -> status::Custom<String> {
     }
 }
 
-#[post("/file/create?<token>", data = "<file_as_str>")]
-pub fn post_file(token: &str, file_as_str: &str) -> status::Custom<String> {
+// when creating a file, the content won't be added in the metadata tree, the content will be directly stored in the file itself
+// so when the user log in his session, it fetch is whole tree which contains only the tree with each encrypted key. If the user the
+#[post("/file/create/<auth_token>", data = "<file_as_str>")]
+pub fn post_file(auth_token: &str, file_as_str: &str) -> status::Custom<String> {
     let generic_error = status::Custom(
         Status::BadRequest,
         "You are not authorized to perform this action".to_string(),
     );
     let success = status::Custom(Status::Ok, "File successfully created".to_string());
 
-    match Database::verify_token(token) {
+    match Database::verify_token(auth_token) {
         Ok(_) => {
             // convert body to struct
             let file: FileEntity = match serde_json::from_str(file_as_str) {
@@ -107,6 +111,9 @@ pub fn post_file(token: &str, file_as_str: &str) -> status::Custom<String> {
             };
 
             if file.create() {
+                // update the tree
+                // get the parent dir and add it into
+
                 return success;
             } else {
                 return generic_error;
@@ -116,8 +123,17 @@ pub fn post_file(token: &str, file_as_str: &str) -> status::Custom<String> {
     }
 }
 
+//todo  get_my_tree?auth_token=
+
+// todo file/get?token={}?path={}
+
+// todo /auth/get_public_key?username={}?auth_token={}
+
+// todo /share/username={}?auth_token={}?path={}?shared_key={}
+
 #[post("/dir/create?<token>", data = "<dir_as_str>")]
 pub fn post_dir(token: &str, dir_as_str: &str) -> status::Custom<String> {
+    let mut new_dir: DirEntity = serde_json::from_str(dir_as_str).unwrap();
     let generic_error = status::Custom(
         Status::BadRequest,
         "You are not authorized to perform this action".to_string(),
@@ -151,6 +167,26 @@ pub fn post_dir(token: &str, dir_as_str: &str) -> status::Custom<String> {
         Err(_) => return generic_error,
     }
 }
+
+/* #[get("/dir/get/<token>", data = "<dir_path>")]
+pub fn get_dir(token: &str, dir_path: &str) -> status::Custom<DirEntity> {
+    let generic_error = status::Custom(
+        Status::BadRequest,
+        "You are not authorized to perform this action".to_string(),
+    );
+    match Database::verify_token(token) {
+        Ok(jwt) => {
+            let user_id = jwt.sub.uid;
+
+            let dir = Database::get_dir(path);
+
+            return generic_error;
+        }
+        Err(_) => return generic_error,
+    }
+} */
+
+// TODO add get_public_key by username
 
 #[post("/auth/change_password?<token>", data = "<user>")]
 pub fn post_change_password(token: &str, user: &str) -> status::Custom<String> {
